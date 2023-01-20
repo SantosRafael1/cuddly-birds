8b2201ec27ae57754f94018919f2c5eda2803d59
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-20 16:24:23 -0300

    Ping a website

---
+++ ping-website/ping_a_website.py
@@ -0,0 +1,14 @@
+from ping3 import ping
+
+hostname = str(input("Host: "))
+
+response_time = ping(hostname)
+if response_time:
+    print(f"\033[32m{hostname} IS up! Response time: {round(response_time, 2)} ms")
+else:
+    print(f"\033[31m{hostname} is DOWN. Response time: {round(response_time, 2)}")
+
+
+
+#It is used to measure the time that a website takes to send packets of data from a source computer
+#to a destination computer.

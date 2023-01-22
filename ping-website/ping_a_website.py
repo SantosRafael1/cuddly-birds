b92a02091075df66664f6494a738aa86668f83e1
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-22 11:36:43 -0300

    Checking if the return is None

--- ping-website/ping_a_website.py
+++ ping-website/ping_a_website.py
@@ -5,6 +5,8 @@ hostname = str(input("Host: "))
 response_time = ping(hostname)
 if response_time:
     print(f"\033[32m{hostname} IS up! Response time: {round(response_time, 2)} ms")
+elif not response_time:
+    print(f"{response_time}")
 else:
     print(f"\033[31m{hostname} is DOWN. Response time: {round(response_time, 2)}")
 
